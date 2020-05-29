5291b065ced66f34138f94dc2f75be00857c04d9
Bob Freid <freidrd@mail.uc.edu>
2020-05-29 18:34:23 -0400

    Added test two for adding to test pipeline

--- app/src/test/java/edu/uc/freidrd/prologger/ExampleUnitTest.kt
+++ app/src/test/java/edu/uc/freidrd/prologger/ExampleUnitTest.kt
@@ -14,4 +14,9 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+    @Test
+    fun addition_isCorrectTwo() {
+        assertEquals(6, 3 + 3)
+    }
+
 }
